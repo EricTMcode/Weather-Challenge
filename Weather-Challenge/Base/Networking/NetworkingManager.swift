9839c3ce9bab5e091f397817194059f50754de0d
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-09-11 14:08:09 +0200

    - Add NetworkingManager to parse the API

--- Weather-Challenge/Base/Networking/NetworkingManager.swift
+++ Weather-Challenge/Base/Networking/NetworkingManager.swift
@@ -6,3 +6,70 @@
 //
 
 import Foundation
+
+final class NetworkingManager {
+    
+    static let shared = NetworkingManager()
+    
+    private init() {}
+    
+    func request<T: Codable>(_ endpoint: Endpoint, type: T.Type?) async throws -> T {
+        
+        guard let url = endpoint.url else { throw NetworkingError.invalidUrl }
+        
+        let request = buildRequest(from: url, methodType: endpoint.methodType)
+        
+        print (request)
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let response = response as? HTTPURLResponse,
+              (200...300) ~= response.statusCode else {
+            let statusCode = (response as! HTTPURLResponse).statusCode
+            throw NetworkingError.invalidStatusCode(statusCode: statusCode)
+        }
+        
+        let decoder = JSONDecoder()
+        let decodedData = try decoder.decode(T.self, from: data)
+        return decodedData
+    }
+}
+
+extension NetworkingManager {
+    enum NetworkingError: LocalizedError {
+        case invalidUrl
+        case invalidStatusCode(statusCode: Int)
+        case invalidData
+        case failedToDecode(error: Error)
+        case custom(error: Error)
+    }
+}
+
+extension NetworkingManager.NetworkingError {
+    var errorDescription: String? {
+        switch self {
+        case .invalidUrl:
+            return "URL isn't valid"
+        case .invalidStatusCode:
+            return "Status code falls into the wrong range"
+        case .invalidData:
+            return "Response data is invalid"
+        case .failedToDecode:
+            return "Failed to decode"
+        case .custom(let err):
+            return "Something went wrong \(err.localizedDescription)"
+        }
+    }
+}
+
+private extension NetworkingManager {
+    func buildRequest(from url: URL, methodType: Endpoint.MethodType) -> URLRequest {
+        var request = URLRequest(url: url)
+        
+        switch methodType {
+        case .GET:
+            request.httpMethod = "GET"
+        }
+        return request
+    }
+}

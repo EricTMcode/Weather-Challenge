d0180cf7de411032a3653e7c743085407dbb0d22
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-09-11 13:54:15 +0200

    Initial Commit

---
+++ Weather-Challenge/Weather_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_ChallengeApp.swift
+//  Weather-Challenge
+//
+//  Created by Eric on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
